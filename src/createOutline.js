039e64383ae8d72c9a0faddc5266607fe29a22af
dymonaz <dymonaz@47f8f282-cd7e-11de-8626-0d8cb8b546e2>
2009-12-19 22:15:34 +0000

    Chrome extension for HTML5 outlining
    git-svn-id: https://h5o.googlecode.com/svn/trunk@15 47f8f282-cd7e-11de-8626-0d8cb8b546e2

---
+++ src/createOutline.js
@@ -0,0 +1,10 @@
+if (window == top) { /* if we loaded a parent page, show the action */
+	chrome.extension.sendRequest({msg : "showAction"});
+	
+	/* listen to requests on this tab to generate outlines */
+	chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
+		if (req.msg == "getOutline") {
+			sendResponse(HTML5Outline(document.body));
+		}
+	});
+}
