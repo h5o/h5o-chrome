141ab2809bc35e0d8a01328b69a27be27744744e
dymonaz <dymonaz@47f8f282-cd7e-11de-8626-0d8cb8b546e2>
2010-01-11 22:20:01 +0000

    Blink section starting element in Chrome, when a link is clicked
    git-svn-id: https://h5o.googlecode.com/svn/trunk@59 47f8f282-cd7e-11de-8626-0d8cb8b546e2

--- src/createOutline.js
+++ src/createOutline.js
@@ -10,8 +10,31 @@ if (window == top) { /* if we loaded a parent page, show the action */
 				break;
 			
 			case "jumpTo":
-				location.href = req.hash;
+				location.href = '#'+req.id;
+				highlight(req.id);
 				break;
 		}
 	});
+	
+	var highlight = function(id)
+	{
+		var el = document.getElementById(id);
+		alert(id);
+		var currentOpacity = window.getComputedStyle(el).opacity;
+		
+		var duration=200, 
+			itr=0;
+		el.style.webkitTransitionProperty="opacity";
+		el.style.webkitTransitionDuration=duration+"ms"
+		el.style.webkitTransitionTimingFunction="ease";
+		var blink = function()
+		{
+			el.style.opacity = (itr % 2 == 0 ? 0 : currentOpacity);
+			if (itr < 3) {
+				itr++;
+				setTimeout(blink, duration);
+			}
+		}
+		blink();
+	}
 }
