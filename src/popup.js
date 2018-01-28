e79877404522476896700bdb58d570c10f398b21
Celti Burroughs <celti@celti.name>
2018-01-28 11:40:36 -0700

    Fix typo in HTML sanitization

--- src/popup.js
+++ src/popup.js
@@ -3,7 +3,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
 	chrome.tabs.sendMessage(tabs[0].id, {msg: "getOutline"}, function(outline)
 	{
 		var elOutline = document.getElementById('outline');
-		elOutline.innerHTML = cDOMPurify.sanitize(outline);
+		elOutline.innerHTML = DOMPurify.sanitize(outline);
 		
 		var links = elOutline.getElementsByTagName('a');
 		for (var i=0; i < links.length; i++) {
