a3677fdecbab9a970da70f24ce299f2d38e10389
Dominykas Blyžė <hello@dominykas.com>
2020-06-03 23:28:28 +0300

    ci: remove crx build
    The crx pre-built extensions can no longer be installed, really, so only the Chrome Store version will be provided going forward.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,13 +23,6 @@ module.exports = function (grunt) {
 				"cwd": 'src/',
 				"src": ["**"],
 				"dest": "dist/extension/"
-			},
-			"crx": {
-				// crx extension has extra permissions and an update URL
-				"expand": true,
-				"cwd": "dist/extension/",
-				"src": ["**"],
-				"dest": "dist/extension-standalone/"
 			}
 		},
 		"compress": {
@@ -41,55 +34,12 @@ module.exports = function (grunt) {
 					"archive": "dist/outliner-v<%= pkg.version %>.zip"
 				}
 			}
-		},
-		"gh-pages": {
-			"options": {
-				base: "dist",
-				add: true,
-				repo: "https://" + process.env.GH_TOKEN + "@github.com/h5o/h5o.github.io.git",
-				branch: "master"
-			},
-			"src": "crx-updates.xml"
-		},
-		"bump": {
-			"options": {
-				commitMessage: 'release %VERSION%',
-				commitFiles: [ "-a" ],
-				tagName: '%VERSION%',
-				tagMessage: 'version %VERSION%',
-				pushTo: 'origin'
-			}
 		}
 	});
 
-	grunt.registerTask("default", "Clean build", ["clean:all", "build", "crx", "compress"]);
+	grunt.registerTask("default", "Clean build", ["clean:all", "build", "compress"]);
 
-	grunt.registerTask("build", "Build unpacked extension", ["gen-manifest", "copy:h5o", "copy:extension", "copy:license", "copy:crx"]);
-
-	grunt.registerTask("crx", "Build the final crx", function () {
-		if (!process.env["H5O_CRX"]) return;
-
-		var manifest = grunt.file.readJSON("dist/extension/manifest.json");
-		manifest["update_url"] = "https://h5o.github.io/crx-updates.xml";
-		manifest["permissions"].push("file://*/*");
-		manifest["content_scripts"][0]["matches"].push("file://*/*");
-		grunt.file.write("dist/extension-standalone/manifest.json", JSON.stringify(manifest, null, "  "));
-
-		var shelljs = require("shelljs");
-		var passphraseEnvVarName = process.env["H5O_CRX_KEY_PASSPHRASE"] ? "H5O_CRX_KEY_PASSPHRASE" : "";
-		var res = shelljs.exec(grunt.template.process("./build/crxmake.sh dist/extension-standalone ./build/h5o-chrome.key " + passphraseEnvVarName));
-		if (res.code !== 0) {
-			grunt.fail.fatal("Failed building the crx");
-		}
-		shelljs.mv("extension-standalone.crx", grunt.template.process("dist/outliner-v<%= pkg.version %>.crx"));
-		grunt.file.write("dist/crx-updates.xml", grunt.template.process(grunt.file.read("./build/crx-updates.xml.ejs"), {
-			data: {
-				appId: "dopdihoblkcnhcniellcadnidfmnalmn",
-				version: manifest.version,
-				downloadLink: "https://github.com/h5o/h5o-chrome/releases/download/" + manifest.version + "/outliner-v" + manifest.version + ".crx"
-			}
-		}));
-	});
+	grunt.registerTask("build", "Build unpacked extension", ["gen-manifest", "copy:h5o", "copy:extension", "copy:license"]);
 
 	grunt.registerTask("gen-manifest", function () {
 
@@ -98,12 +48,6 @@ module.exports = function (grunt) {
 		manifest.author = grunt.config.get("pkg.author");
 		grunt.file.write("dist/extension/manifest.json", JSON.stringify(manifest, null, "  "));
 
-	});
-
-	grunt.registerTask("release", function () {
-		var bump = grunt.option("bump");
-		if (bump != "patch" && bump != "minor" && bump != "major") grunt.fail.fatal("Please pass --bump");
-		grunt.task.run(["checkbranch:master", "checkpending", "bump:" + bump]);
 	});
 
 };
